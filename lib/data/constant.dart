3258e9209ff6a0f7fa7327b244db3a8ea5d65fa6
Raed-It <raedswanit@gmail.com>
2024-02-27 15:27:21 +0300

    Helper instance

--- lib/data/constant.dart
+++ lib/data/constant.dart
@@ -3,6 +3,9 @@ import 'package:helper/data/models/dio_config_model.dart';
 class HelperConstant {
   HelperConstant._();
 
+//single toon class
+  static final HelperConstant instance = HelperConstant._();
+
   static const double fontSize = 15;
   static String? token;
   static String local = "ar";
--- lib/helper.dart
+++ lib/helper.dart
@@ -1,4 +1,16 @@
+import 'package:helper/data/models/dio_config_model.dart';
+
+import 'data/constant.dart';
+
 export 'package:helper/data/enums/request_type.dart';
 export 'package:helper/data/enums/api_call_status.dart';
 export 'package:helper/utility/base_client.dart';
-class Helper {}
+
+class Helper {
+  static void initial(
+      {String? appName, String local = "ar", ApiConfig? apiConfig}) {
+    HelperConstant.instance;
+    HelperConstant.local = local;
+    HelperConstant.apiConfig = apiConfig ?? ApiConfig.printResponse();
+  }
+}
